23ce478c4edb29daf43415d9275274005c065f94
xavier <xuanchenxiangzzz@163.com>
2018-12-13 11:22:39 +0800

    初始化

---
+++ material/src/main/java/com/xavier/material/MaterialApplication.java
@@ -0,0 +1,14 @@
+package com.xavier.material;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaterialApplication.class, args);
+    }
+
+}
+
